d7d5f45fbf1949646358a6261c4e0cb1e41188b9
Maxorik <gms441@mail.ru>
2019-05-27 13:08:05 +0700

    add enter form and toggle

---
+++ scripts.js
@@ -0,0 +1,13 @@
+function showregform(){
+    var regform = document.getElementById('reg_form');
+    var enterform = document.getElementById('enter_form');
+    regform.style.display = 'block';
+    enterform.style.display = 'none'; 
+}
+
+function showenterform(){
+    var regform = document.getElementById('reg_form');
+    var enterform = document.getElementById('enter_form');
+    regform.style.display = 'none';
+    enterform.style.display = 'block';
+}
