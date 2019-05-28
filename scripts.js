c19781f69594b6ec5737d414170f0d01c4d2466d
Maxorik <gms441@mail.ru>
2019-05-28 13:39:36 +0700

    add checking of the login form

--- scripts.js
+++ scripts.js
@@ -11,3 +11,25 @@ function showenterform(){
     regform.style.display = 'none';
     enterform.style.display = 'block';
 }
+
+function checktheform(){
+    var enterform = document.getElementById('enter_form');
+    var header_buttons = document.getElementById('header_buttons');
+    var email = document.getElementById('email');
+    var pass = document.getElementById('pass');
+    var header_username = document.getElementById('header_username');
+    
+    if (email.value == "" ){
+        email.classList.add('placeholder-alert');
+    }
+    if (pass.value == "" ){
+        pass.classList.add('placeholder-alert');
+    }
+    else{
+        enterform.style.display = 'none';
+        header_buttons.style.display = 'none';
+        header_username.style.display = 'block';
+        header_username.classList.add('header_nav__elem');
+        header_username.textContent = email.value;
+    }
+}
