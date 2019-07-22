6f99e6a64d37f01a6bdebe84361805a5768060ef
Maxorik <gms441@mail.ru>
2019-07-22 16:06:13 +0700

    fix day

--- src/js/buttons.js
+++ src/js/buttons.js
@@ -5,6 +5,12 @@ var form_enter = document.getElementsByClassName('form_enter');
 var btn_enter = document.getElementsByClassName('btn_enter');
 var btn_login = document.getElementsByClassName('btn_login');
 
+const _form_enter = document.getElementById('_form_enter');
+const _form_reg = document.getElementById('_form_reg');
+
+_form_enter.querySelector('.btn_login').addEventListener('click', form_confirm, false);
+_form_reg.querySelector('.btn_login').addEventListener('click', form_confirm, false);
+
 for(var i=0; i<btn_reg.length; i++){
     btn_reg[i].addEventListener('click', show_reg, false);
 }
@@ -13,9 +19,9 @@ for(var i=0; i<btn_enter.length; i++){
     btn_enter[i].addEventListener('click', show_enter, false);
 }
 
-for(var i=0; i<btn_login.length; i++){
+/*for(var i=0; i<btn_login.length; i++){
     btn_login[i].addEventListener('click', login_user, false);
-}
+}*/
 
 function show_reg(){
     for(var i=0; i<form_enter.length; i++){
@@ -37,8 +43,10 @@ function show_enter(){
     }
 }
 
-function login_user(){
-    var log_inputs = form_enter[0].getElementsByClassName('input');
+function form_confirm(event){
+    var target = event.target;
+    var the_form = target.parentElement;
+    var log_inputs = the_form.getElementsByClassName('input');
     for(var i=0; i<log_inputs.length; i++){
         if(log_inputs[i].value == ''){
             log_inputs[i].classList.add('input_empty');
@@ -47,8 +55,9 @@ function login_user(){
             log_inputs[i].classList.remove('input_empty');
         }
     }
-    var false_inp = form_enter[0].getElementsByClassName('input_empty');
+    var false_inp = the_form.getElementsByClassName('input_empty');
     if(false_inp.length == 0){
-        document.location.href = "/page2.html";
-    }
+        document.location.href = "page2.html";
+    } 
 }
+
